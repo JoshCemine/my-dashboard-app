4bd2c1bc48c2f850bf12e97fd298adff1072b5a2
Josh Cemine <j.jcemine@gmail.com>
2025-09-19 00:45:07 +0800

    losing my mind

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
-import { auth } from "@/auth"
-import { NextResponse } from "next/server"
+export { auth as middleware } from "@/auth"
+// import { auth as middleware } from "@/auth"
+// import { NextResponse } from "next/server"
 
 export const config = {
     matcher: [
@@ -7,22 +8,22 @@ export const config = {
     ]
 }
 
-export default auth((req) => {
-    const { pathname } = req.nextUrl
-    const isLoggedIn = !!req.auth
+// export default auth((req) => {
+//     const { pathname } = req.nextUrl
+//     const isLoggedIn = !!req.auth
 
-    // If user is not logged in and trying to access protected routes
-    if (!isLoggedIn && pathname.startsWith("/dashboard")) {
-        const loginUrl = new URL("/login", req.url)
-        loginUrl.searchParams.set("callbackUrl", pathname)
-        return NextResponse.redirect(loginUrl)
-    }
+//     // If user is not logged in and trying to access protected routes
+//     if (!isLoggedIn && pathname.startsWith("/dashboard")) {
+//         const loginUrl = new URL("/login", req.url)
+//         loginUrl.searchParams.set("callbackUrl", pathname)
+//         return NextResponse.redirect(loginUrl)
+//     }
 
-    // If user is logged in and trying to access login page
-    if (isLoggedIn && pathname.startsWith("/login")) {
-        const dashboardUrl = new URL("/dashboard", req.url)
-        return NextResponse.redirect(dashboardUrl)
-    }
+//     // If user is logged in and trying to access login page
+//     if (isLoggedIn && pathname.startsWith("/login")) {
+//         const dashboardUrl = new URL("/dashboard", req.url)
+//         return NextResponse.redirect(dashboardUrl)
+//     }
 
-    return NextResponse.next()
-})
+//     return NextResponse.next()
+// })
